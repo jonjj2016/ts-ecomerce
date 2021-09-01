1a1cb328621749b40ce0ece873fee8e3c3c95c7f
jonjj2016 <jonmartirosyan1988@gmail.com>
2021-09-01 13:33:07 +0800

    added some learning code

--- playground.ts
+++ playground.ts
@@ -1,10 +1,14 @@
-
-
+// type aliases 
+type Data = string;
 interface Person {
-    name: string,
+    name: Data,
     age: number
 }
-
+// interface PersonLoggerFn {
+//     (name: string , age: number): string
+// }
+// descibe function types
+type PersonLoggerFn = (name: string , age: number) => string
 // type Person ={
 //     name: string,
 //     age: number,
@@ -16,20 +20,25 @@ const personObject: Person ={
     name: 'Jon Martirosyan',
     age: 33,
 }
-    const logPersonInfo = (personName: string, personAge: number) => {
+    const logPersonInfo: PersonLoggerFn = (personName: string, personAge: number) => {
         const info =`Name: ${personName}, age: ${personAge}`;
         console.log(info);
         
         return info
 
-    };
+    }
+    // implecit types 
+    const implicitFn = (name: string):string =>{
+        return name
+    }
+    implicitFn('Armena');
     const logpersonInfo2 = (person: Person) => {
         const info =`Name: ${person.name}, age: ${person.age}`;
         console.log(info);
         
         return info
     }
-    logPersonInfo(name, age);
-    logpersonInfo2(personObject)
+    const log = logPersonInfo(name, age);
+    const ko: string = logpersonInfo2(personObject)
 }
 export default playfunc;
